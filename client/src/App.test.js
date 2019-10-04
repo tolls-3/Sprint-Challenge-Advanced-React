d4c0dc6d7f6dea5a0977da82ea372b06a8f02a1b
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-10-04 10:32:53 +0100

    final submission, dead laptop

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import * as rtl from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import App from './App';
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import App from "./App";
+// import NavBar from "./Components/NavigationBar";
 
-
-it('renders without crashing', () => {
+it("renders without crashing", () => {
   const wrapped = rtl.render(<App />);
   const heading = wrapped.queryByText(/Womens World/i);
   expect(heading).toBeTruthy();
   expect(heading).toBeVisible();
 });
+it("renders players in app", () => {
+  const app = rtl.render(<App />);
+  app.getByText(/players/i);
+  console.log(app.debug());
+});
+// describe("NavBar", () => {
+//   it("NavBar renders without crashing", () => {
+//     const div = document.createElement("div");
+//     ReactDOM.render(<NavBar />, div);
+//   });
+// });
