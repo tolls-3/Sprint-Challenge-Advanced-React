c3a9c96bdf33d2be5d3b348a7bddb9c0bc59a9de
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-10-04 09:01:48 +0100

    created card components ad installed semantic ui

--- client/src/Components/PlayersCard.js
+++ client/src/Components/PlayersCard.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Card, Icon } from "semantic-ui-react";
-//import styled from "styled-components";
+import styled from 'styled-components';
 
 const PlayersCard = ({ key, name, country, searches }) => {
   return (
     <div>
-      <div>
+      <FollowersCards>
         <Card.Header>
           <h2>Name: {name}</h2>
         </Card.Header>
@@ -18,9 +18,29 @@ const PlayersCard = ({ key, name, country, searches }) => {
             Searches: {searches}
           </h3>
         </Card.Description>
-      </div>
+      </FollowersCards>
     </div>
   );
 };
 export default PlayersCard;
+
+const FollowersCards = styled.div`
+  width: 50%;
+  padding: 20px;
+  display: flex;
+  flex-direction:column;
+  border-radius: 5px;
+  box-shadow: 0 1px 6px -2px #000;
+  background-color: #fff;
+  margin-bottom: 30px;
+  align-items:center;
+  :hover {
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+  }
+  
+  }
+  h3 {
+    font-size: 1.4rem;
+  }
+`;
 
